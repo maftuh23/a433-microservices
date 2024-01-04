8b3bff36ef1affb2400e908fb728b1f5ca8bfed5
Maftuh Ichsan <maff@Maftuhs-MacBook-Air.local>
2024-01-04 18:47:50 +0700

    asd

--- build_push_image.sh
+++ build_push_image.sh
@@ -5,6 +5,6 @@ docker build -t item-app:v1 .
 docker images
 
 # Push docker images ke docker hub
-echo $PASSWORD_DOCKER_HUB | docker login -u maftuh23 --password-stdin
 docker tag item-app:v1 maftuh23/item-app:v1
+echo $PASSWORD_DOCKER_HUB | docker login -u maftuh23 --password-stdin
 docker push maftuh23/item-app:v1
